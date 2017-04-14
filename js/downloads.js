d01b6353b9f459b0f89d1c11e1671c4cb1a61587
WaDesiGh <youthcrew96@gmail.com>
2017-04-15 00:55:10 +0700

    Update downloads.js

--- js/downloads.js
+++ js/downloads.js
@@ -23,3 +23,7 @@ var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElem
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
+function a_to_fa()
+{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+protected_links = "waroengdesigngraphic.blogspot.com,paypal.com,dmca.com,creativecommons.org,youtube.com,facebook.com,google.com";
+auto_safelink();
